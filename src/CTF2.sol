d899d37e10590db07ee0476b6bb80b9c81af61ce
brandon engstrom <bengstrom32@gmail.com>
2023-10-22 15:59:41 -0400

    first commit

--- src/CTF2.sol
+++ src/CTF2.sol
@@ -20,7 +20,6 @@ function solution(uint256[10] calldata unsortedArray) external returns (uint256[
 contract CTF2 is Isolution2 {
 
     // function in Solidity before being converted to YUL
-    // COMMENT OUT BEFORE DEPLOYMENT
     function solutionNotOptimized( uint256[10] calldata unsortedArray) 
         external pure returns (uint256[10] memory sortedArr) {
         
@@ -48,8 +47,9 @@ contract CTF2 is Isolution2 {
     function solution(uint256[10] calldata unsortedArray) 
         external pure returns (uint256[10] memory sortedArr) {
         assembly {
-            let n := 10  // Length of the array
-            // Set the base address for the sorted array to the memory slot of sortedArr
+            // length of the fixed array is 10
+            let n := 10  
+            // sets the base address for the sorted array to the memory slot of sortedArr at 0x20
             let sortedArrPtr := add(sortedArr, 0x20) 
 
             // Copy unsorted array to sorted array
@@ -65,13 +65,41 @@ contract CTF2 is Isolution2 {
                 mstore(add(arr, mul(j, 0x20)), temp)
             }
 
-            // First pass
-            if gt(mload(add(sortedArr, 0x00)), mload(add(sortedArr, 0x20))) { swap(sortedArr, 0, 1) }
-            // ... continue for all adjacent pairs and passes as in your original function
+            // to bubble sort without a for loop there will be 9 passes with 9 steps each 
+            // each step will the largest element bubbles up to its correct position.
+            // for example in the first step the largest element bubbles up to its correct position at index 9.
+            // then at the second step the second largest element bubbles up to its correct position at index 8.
+            // this process continues for an array fixed at a length of 10.
 
-            // ... continue for a total of 9 passes, repeating the above comparisons
+            // in each line first the first 2 memory locations and loads them from the memory positions.
+            // then the two elements are compared and swapped if the first element is greater than the second element. 
 
-            // Ninth pass
+            // first pass
+            if gt(mload(add(sortedArrPtr, 0x00)), mload(add(sortedArrPtr, 0x20))) { swap(sortedArrPtr, 0, 1) }
+            if gt(mload(add(sortedArrPtr, 0x20)), mload(add(sortedArrPtr, 0x40))) { swap(sortedArrPtr, 1, 2) }
+            if gt(mload(add(sortedArrPtr, 0x40)), mload(add(sortedArrPtr, 0x60))) { swap(sortedArrPtr, 2, 3) }
+            if gt(mload(add(sortedArrPtr, 0x60)), mload(add(sortedArrPtr, 0x80))) { swap(sortedArrPtr, 3, 4) }
+            if gt(mload(add(sortedArrPtr, 0x80)), mload(add(sortedArrPtr, 0xa0))) { swap(sortedArrPtr, 4, 5) }
+            if gt(mload(add(sortedArrPtr, 0xa0)), mload(add(sortedArrPtr, 0xc0))) { swap(sortedArrPtr, 5, 6) }
+            if gt(mload(add(sortedArrPtr, 0xc0)), mload(add(sortedArrPtr, 0xe0))) { swap(sortedArrPtr, 6, 7) }
+            if gt(mload(add(sortedArrPtr, 0xe0)), mload(add(sortedArrPtr, 0x100))) { swap(sortedArrPtr, 7, 8) }
+            if gt(mload(add(sortedArrPtr, 0x100)), mload(add(sortedArrPtr, 0x120))) { swap(sortedArrPtr, 8, 9) }
+
+            // second pass
+
+            // third pass
+
+            // fourth pass
+
+            // fifth pass
+
+            // sixth pass
+
+            // seventh pass
+
+            // eigth pass
+
+            // ninth pass
             if gt(mload(add(sortedArr, 0x00)), mload(add(sortedArr, 0x20))) { swap(sortedArr, 0, 1) }
             if gt(mload(add(sortedArr, 0x20)), mload(add(sortedArr, 0x40))) { swap(sortedArr, 1, 2) }
             // ... continue for all adjacent pairs
