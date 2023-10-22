7e98b92ae08261c282df85572b3b704dd4556d90
brandon engstrom <bengstrom32@gmail.com>
2023-10-22 11:26:51 -0400

    first commit

--- src/CTF2.sol
+++ src/CTF2.sol
@@ -3,7 +3,12 @@
 // the following interface:
 
 
-
+/**
+ * @title 
+ * @author 
+ * @notice uses a bubble sort method then reduces the operations to yul to maximize efficiency
+ * try without the for loop also 
+ */
 
 //SPDX-License-Identifier:MIT
 
@@ -17,15 +22,25 @@ function solution(uint256[10] calldata unsortedArray) external returns (uint256[
 contract CTF2 is Isolution2 {
 
 
-    function solution(
-        uint256[10] calldata unsortedArray
-    ) external pure override returns (uint256[10] memory sortedArray) {
+    function solution( uint256[10] calldata unsortedArray) 
+        external pure override returns (uint256[10] memory sortedArr) {
         
-        uint256[10] memory sortedArray;  
-        
-       
+            uint n = unsortedArray.length;
 
-        return sortedArray;
+            uint[10] memory sortedArr = unsortedArray;
+
+            // Traverse through all elements in the array
+            for(uint i = 0; i < n; i++) {
+                // Last i elements are already in place, so skip them
+                for(uint j = 0; j < n-i-1; j++) {
+                    // Swap if the element found is greater than the next element
+                    if(sortedArr[j] > sortedArr[j+1]) {
+                        (sortedArr[j], sortedArr[j+1]) = (sortedArr[j+1], sortedArr[j]);
+                    }
+                }
+            }
+            return sortedArr;        
+        
     }
 
 }
