c57f12642998c09c67673ea60f66226077506078
brandon engstrom <bengstrom32@gmail.com>
2023-10-22 15:26:11 -0400

    first commit

--- src/CTF2.sol
+++ src/CTF2.sol
@@ -20,6 +20,7 @@ function solution(uint256[10] calldata unsortedArray) external returns (uint256[
 contract CTF2 is Isolution2 {
 
     // function in Solidity before being converted to YUL
+    // COMMENT OUT BEFORE DEPLOYMENT
     function solutionNotOptimized( uint256[10] calldata unsortedArray) 
         external pure returns (uint256[10] memory sortedArr) {
         
@@ -42,72 +43,42 @@ contract CTF2 is Isolution2 {
     }
 
 
-    // The function after using YUL for the login in the nested for loop
-    function solution(uint256[10] calldata unsortedArray)
-        external pure override returns(uint256[10] memory sortedArray) {
 
-            uint[10] memory sortedArr = unsortedArray;
+    // Optimized manually
+    function solution(uint256[10] calldata unsortedArray) 
+        external pure returns (uint256[10] memory sortedArr) {
+        assembly {
+            let n := 10  // Length of the array
+            // Set the base address for the sorted array to the memory slot of sortedArr
+            let sortedArrPtr := add(sortedArr, 0x20) 
 
-            assembly {
-                // length of the array
-                let n := 10
-
-                for { let i := 0 } lt(i, sub(n, 1)) { i := add(i, 1) }
-                {
-                    for { let j := 0 } lt(j, sub(sub(n, i), 1)) { j := add(j, 1) }
-                    {
-                        // logic
-                        
-
-                    }
-                }
+            // Copy unsorted array to sorted array
+            for { let i := 0 } lt(i, n) { i := add(i, 1) } {
+                let elem := mload(add(add(unsortedArray, 0x20), mul(i, 0x20)))
+                mstore(add(sortedArr, mul(i, 0x20)), elem)
             }
 
+            // Helper function to swap two elements in the array
+            function swap(arr, i, j) {
+                let temp := mload(add(arr, mul(i, 0x20)))
+                mstore(add(arr, mul(i, 0x20)), mload(add(arr, mul(j, 0x20))))
+                mstore(add(arr, mul(j, 0x20)), temp)
+            }
+
+            // First pass
+            if gt(mload(add(sortedArr, 0x00)), mload(add(sortedArr, 0x20))) { swap(sortedArr, 0, 1) }
+            // ... continue for all adjacent pairs and passes as in your original function
+
+            // ... continue for a total of 9 passes, repeating the above comparisons
+
+            // Ninth pass
+            if gt(mload(add(sortedArr, 0x00)), mload(add(sortedArr, 0x20))) { swap(sortedArr, 0, 1) }
+            if gt(mload(add(sortedArr, 0x20)), mload(add(sortedArr, 0x40))) { swap(sortedArr, 1, 2) }
+            // ... continue for all adjacent pairs
+
+            // No additional steps needed to return sortedArr as it's already the name of the return variable
         }
-
-
-
-
-        // Optimized manually
-        function solutionNotOptimized(uint256[10] calldata unsortedArray) 
-            external pure returns (uint256[10] memory sortedArr) {
-                assembly {
-                    let n := 10  // Length of the array
-                    let sortedArr := add(msize(), 1)  // Base address for the sorted array
-
-                    // Copy unsorted array to sorted array
-                    for { let i := 0 } lt(i, n) { i := add(i, 1) } {
-                        let elem := mload(add(add(unsortedArray, 0x20), mul(i, 0x20)))
-                        mstore(add(sortedArr, mul(i, 0x20)), elem)
-                    }
-
-                    // Helper function to swap two elements in the array
-                    function swap(arr, i, j) {
-                        let temp := mload(add(arr, mul(i, 0x20)))
-                        mstore(add(arr, mul(i, 0x20)), mload(add(arr, mul(j, 0x20))))
-                        mstore(add(arr, mul(j, 0x20)), temp)
-                    }
-
-                    // First pass
-                    if gt(mload(add(sortedArr, 0x00)), mload(add(sortedArr, 0x20))) { swap(sortedArr, 0, 1) }
-                    if gt(mload(add(sortedArr, 0x20)), mload(add(sortedArr, 0x40))) { swap(sortedArr, 1, 2) }
-                    // ... continue for all adjacent pairs
-
-                    // ... continue for a total of 9 passes, repeating the above comparisons
-
-                    // Ninth pass
-                    if gt(mload(add(sortedArr, 0x00)), mload(add(sortedArr, 0x20))) { swap(sortedArr, 0, 1) }
-                    if gt(mload(add(sortedArr, 0x20)), mload(add(sortedArr, 0x40))) { swap(sortedArr, 1, 2) }
-                    // ... continue for all adjacent pairs
-
-                    // Load sorted array into return variable
-                    for { let i := 0 } lt(i, n) { i := add(i, 1) } {
-                        let elem := mload(add(sortedArr, mul(i, 0x20)))
-                        mstore(add(sortedArr, add(0x20, mul(i, 0x20))), elem)
-                    }
-                }
-}
-
+    }
 
 
 }
